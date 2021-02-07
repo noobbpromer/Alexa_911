1c543508e4e0e34de85ce8342455ba70465bf925
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-02-07 21:39:00 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -156,7 +156,7 @@ class AccidentIntentHandler(AbstractRequestHandler):
         if not quiz_started:
             
             current_question_index = 0
-            question = car_accident[current_question_index]["q"]
+            q_for_car = car_accident[current_question_index]["q"]
             # speak_output = ("<break time='0.5s'/> {}").format(question)
             
 
