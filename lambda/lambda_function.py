f2287a15164e30b75cabf0aa9261b966409961aa
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-02-07 22:23:04 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -159,7 +159,7 @@ class AccidentIntentHandler(AbstractRequestHandler):
         else:
             current_question_index = 1
             question = accident_data[current_question_index]["q"]
-            speak_output = ("<break time='0.5s'/> {location} {}").format(question,location=location)
+            speak_output = ("{location}").format(question,location=location)
             
         session_attributes["current_question_index"] = current_question_index
         session_attributes["question"] = question
