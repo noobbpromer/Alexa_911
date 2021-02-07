76cec7d746b8e2fa89bed66add2fac1d18f27a2c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-02-07 22:08:48 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -152,15 +152,21 @@ class AccidentIntentHandler(AbstractRequestHandler):
         # quiz_started = session_attributes["quiz_started"]
         slots = handler_input.request_envelope.request.intent.slots
         location = slots["location"].value
-
-        current_question_index = 0
-        question = accident_data[current_question_index]["q"]
-        speak_output = ("<break time='0.5s'/> {}").format(question)
+        if location ==None:
+            current_question_index = 0
+            question = accident_data[current_question_index]["q"]
+            speak_output = ("<break time='0.5s'/> {}").format(question)
+        else:
+            current_question_index = 1
+            question = accident_data[current_question_index]["q"]
+            speak_output = ("<break time='0.5s'/> {}").format(question)
+            
             
 
 
         session_attributes["current_question_index"] = current_question_index
         session_attributes["question"] = question
+        
         # session_attributes["quiz_started"] = True
         # quiz_started=True
 
