c00e54e8bb8bea24754cea4a8fc431da209aa926
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-02-18 04:06:05 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -148,8 +148,22 @@ class AnswerIntentHandler(AbstractRequestHandler):
                 .ask(speak_output)
                 .response
         )
+class OutlookIntentHandler(AbstractRequestHandler):
+    """Handler for Help Intent."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        return is_intent_name("OutlookIntent")(handler_input)
 
-    
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "You can say hello to me! How can I help?"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
     
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
@@ -257,6 +271,7 @@ sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(AnswerIntentHandler())
+sb.add_request_handler(OutlookIntentHandler())
 sb.add_request_handler(LocationIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
