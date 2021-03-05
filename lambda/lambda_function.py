f868ebcb58da77dd11de86a03a17650a170e5bdd
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-03-05 18:26:41 +0000

    updated lambda/car_accident.json, lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -81,7 +81,7 @@ class LocationIntentHandler(AbstractRequestHandler):
             question = accident_data[current_question_index]["q"]
             speak_output = ("{prepositions} {location} ? {}").format(question,prepositions=prepositions,location=location)
             
-            if (someone ==None) or (verb ==None) or (incident ==None):
+            if (someone ==None) or (verb ==None) or (incident ==None) :
                 
                 speak_output = ("nice")
             
