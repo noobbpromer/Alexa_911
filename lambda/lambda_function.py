decf00af36a9ce684fd37da9edf0a68ce3f739f8
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-03-05 21:59:22 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -84,7 +84,7 @@ class LocationIntentHandler(AbstractRequestHandler):
             question = accident_data[current_question_index]["q"]
             speak_output = ("{prepositions} {location} ? {}").format(question,prepositions=prepositions,location=location)
             
-            if (someone ==None) or (verb ==None) or (incident ==None) and (if_enter==1):
+            if (someone ==None) or (verb ==None) or (incident ==None) and (a==1):
                 
                 
                 current_question_index = 2
