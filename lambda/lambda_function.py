a6e25648791dd078a058a483f85275b62adb4cb9
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-03-10 01:39:57 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -77,9 +77,10 @@ class LocationIntentHandler(AbstractRequestHandler):
             if_enter=0
             counter+=1
             
-            
-            question = accident_data[current_question_index]["q"]
-            speak_output = (" {counter} {}").format(question,counter=counter)
+            if counter ==1:
+                
+                question = accident_data[current_question_index]["q"]
+                speak_output = ("{}").format(question)
             
             if (someone ==None) or (verb ==None) or (incident ==None):
                 speak_output = ("I'm sorry, I didn't get that. if you have emergency, Could you please tell me what the incident was again?")
