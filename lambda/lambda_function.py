67c4da2f263bcc77e8f99bd0e93a66bfc85c470c
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-03-12 05:01:07 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -161,7 +161,7 @@ class InjuriesIntentHandler(AbstractRequestHandler):
         
         # situation=slots["situation"].value
         what_happen=slots["what_happen"].value
-        if 'i dont konw' in what_happen:
+        if 'konw' in what_happen:
             
             speak_output = ("{what_happen}").format(what_happen=what_happen)
             
