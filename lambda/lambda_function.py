026d8b58bf43ded0d0e6fbf5f7764e8f2df408af
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-04-09 04:27:10 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -209,7 +209,7 @@ class InjuriesIntentHandler(AbstractRequestHandler):
         verb= list(verb.split(" "))
         status=list(status.split(" "))
         
-        to_speak=subject[0]+verb[0]+status[0]
+        to_speak=subject[1]+verb[1]+status[1]
         
         
         speak_output=("{to_speak}").format(to_speak=to_speak)
